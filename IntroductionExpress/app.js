07cbd48288447a5ac9cf8562762ab163920decda
bsihame <sihamebazi@pursuit.org>
2020-01-14 18:48:18 -0500

    create a server

--- IntroductionExpress/app.js
+++ IntroductionExpress/app.js
@@ -4,6 +4,13 @@ const express  = require("express");
 const app = express();
 //declare a port
 const port = 3000;
+//get request
+      //get request: getting data
+      //post request:post request submitting 
+      //put request: updating all data
+      //patch updating specific data
+      //delete deleting data
+app.get
 //app.listen takes a port and callback function
 app.listen(port, () =>{
   console.log("Server is running on port", port)
